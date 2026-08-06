4e6ced723b20a80334e3aea5f1471fbc0395df21
Sebastian Arellano <sebastian.arellanoscavone@yahoo.com>
2026-08-06 11:32:42 -0300

    Adding Complete feature

--- web.py
+++ web.py
@@ -1,4 +1,3 @@
-import streamlit
 import streamlit as sl
 import funciones as fn
 
@@ -6,29 +5,26 @@ import funciones as fn
 todos = fn.get_todos()
 
 def add_todo():
-    todo = sl.session_state['new_todo'] + "\n"
-    todos.append(todo)
-    fn.write_todos(todos)
-
-
-
+    if sl.session_state['new_todo'] != "":
+        todo_local = sl.session_state["new_todo"] + '\n'
+        if todo_local not in todos:  # Add this if-condition to fix the issue
+            todos.append(todo_local)
+            fn.write_todos(todos)
+            sl.session_state['new_todo'] = ""
 
 sl.title("Todo App")
 sl.subheader("This is my Todo App")
 sl.write("Some other text.")
 
-
-
-
-
-
-for todo in todos:
-    sl.checkbox(todo)
-
+for index, todo in enumerate(todos):
+    checkbox = sl.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        fn.write_todos(todos)
+        del sl.session_state[todo]
+        sl.rerun()
 
 sl.text_input(label="",
               placeholder="Write your todo here",
               on_change=add_todo,
               key="new_todo")
-
-sl.session_state
